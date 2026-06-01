c255ef7cab1519af134b8548efe2749aaf6e7c1b
Ekamjot Singh <ekam3372@gmail.com>
2026-06-01 13:37:44 +0530

    fix: call django.setup() before importing routing in asgi.py

--- nexus_project/asgi.py
+++ nexus_project/asgi.py
@@ -1,6 +1,8 @@
 import os
+import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'nexus_project.settings')
+django.setup()
 
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
